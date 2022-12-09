feb1c9551224d1f671b7a5b5af1b4b7361f1ad2f
Tinashe Makarudze <tinashekmakarudze@gmail.com>
2022-12-09 02:59:39 +0200

    changes

--- assistant/actions/actions.py
+++ assistant/actions/actions.py
@@ -11,32 +11,11 @@ import keyword
 from typing import Any, Text, Dict, List
 from urllib import request
 
-from rasa_sdk import Action, Tracker, events, FormValidationAction 
+from rasa_sdk import Action, Tracker, events
 from rasa_sdk.executor import CollectingDispatcher
 import webbrowser
-from rasa_sdk.types import DomainDict
 
 apiUrl = "localhost:4000"
-
-
-class CreateInvoice(Action):
-    def name(self) -> Text:
-        return "validate_add_funds_form"
-
-    def validate_amount(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        """Validate `amount` value."""
-
-        if slot_value < "10":
-            dispatcher.utter_message(text=f"Minimum deposit is $10")
-            return {"amount": None}
-        dispatcher.utter_message(text=f"OK! You want to add ${slot_value} to your account")
-        return {"amount": slot_value}
 
 
 class ActionGetBalance(Action):
@@ -52,30 +31,62 @@ class ActionGetBalance(Action):
         return []
         # return [events.SlotSet("account_type", data["account_type"])]
 
-
-
-class ActionHelloWorld(Action):
-
+class CreateInvoice(Action):
     def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-
-class ActionVideo(Action):
-    def name(self) -> Text:
-        return "action_video"
+        return "action_create_invoice"
 
     def run(self, dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        video_url="https://youtu.be/OUpL5nxKI4A"
-        dispatcher.utter_message(text="Wait... Playng a video!")
-        webbrowser.open(video_url)
+        amount = tracker.get_slot('amount')
+
+        if not amount:
+            dispatcher.utter_message("How much do you want to add?")
+            return []
+        # url = "http://google.com"
+        dispatcher.utter_message(text="{amount} ready for payment.")
         return []
+        # return [events.SlotSet("account_type", data["account_type"])]
+
+class ActionOrderStatus(Action):
+    def name(self) -> Text:
+        return "action_order_status"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # url = "http://google.com"
+        dispatcher.utter_message(text="Your order is paid. Expect your order in 2 days time. Your pickup location is Eastgate Mall")
+        return []
+        # return [events.SlotSet("account_type", data["account_type"])]
+
+
+
+
+# class ActionHelloWorld(Action):
+
+#     def name(self) -> Text:
+#         return "action_hello_world"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(text="Hello World!")
+
+#         return []
+
+# class ActionVideo(Action):
+#     def name(self) -> Text:
+#         return "action_video"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         video_url="https://youtu.be/OUpL5nxKI4A"
+#         dispatcher.utter_message(text="Wait... Playng a video!")
+#         webbrowser.open(video_url)
+#         return []
